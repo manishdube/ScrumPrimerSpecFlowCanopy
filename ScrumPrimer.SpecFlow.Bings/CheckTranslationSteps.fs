e54d990e5723169dc9b64564eeac4b3325b85808
Steven Mak <stevenmak@gmail.com>
2014-06-18 18:54:04 +0800

    First test done

---
+++ ScrumPrimer.SpecFlow.Bings/CheckTranslationSteps.fs
@@ -0,0 +1,20 @@
+﻿[<TechTalk.SpecFlow.Binding>]
+module CheckTranslationSteps
+
+open TechTalk.SpecFlow
+open NUnit.Framework
+open canopy
+open System
+
+
+let [<Given(@"I am on the home page")>] GivenIAmOnTheHomePage() =
+    configuration.chromeDir <- @"D:\"
+    start chrome
+    url "http://www.scrumprimer.org"
+
+let [<When(@"I go to scrum primer overview translation page")>] WhenIGoToScrumPrimerOverviewTranslationPage() =
+    click "Translations"
+
+let [<Then>] ``I will see scrum primer overview in (.*)`` (language : string) =
+    Assert.AreEqual((element "#overview_pic_1").GetAttribute("href"), @"http://www.scrumprimer.org/overview/" + language + "_scrum_overview1.pdf")
+    quit()
---
+++ ScrumPrimer.SpecFlow/CheckTranslationSteps - Copy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace ScrumPrimer.SpecFlow
+{
+    [Binding]
+    public class CheckTranslationSteps
+    {
+        [Given(@"I am on the home page")]
+        public void GivenIAmOnTheHomePage()
+        {
+        }
+
+        [When(@"I go to scrum primer overview translation page")]
+        public void WhenIGoToScrumPrimerOverviewTranslationPage()
+        {
+        }
+
+        [Then(@"I will see scrum primer overview in zh-cn")]
+        public void ThenIWillSeeScrumPrimerOverviewInZh_Cn()
+        {
+            Assert.AreEqual(1,2);
+        }
+    }
+}
